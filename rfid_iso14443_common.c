7e38d74fdd2f701755117714b6c5bf4f836c1245
Harald Welte <laforge@gnumonks.org>
2005-05-29 18:21:58 +0000

    add gpl disclaimers
    git-svn-id: https://svn.gnumonks.org/trunk/librfid@1182 e0336214-984f-0b4b-a45f-81c69e1f0ede

--- rfid_iso14443_common.c
+++ rfid_iso14443_common.c
@@ -1,3 +1,20 @@
+
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 
+ *  as published by the Free Software Foundation
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
 static unsigned int fsdi_table[] = { 16, 24, 32, 40, 48, 64, 96, 128, 256 };
 
 int iso14443_fsdi_to_fsd(unsigned int *fsd, unsigned char fsdi)
