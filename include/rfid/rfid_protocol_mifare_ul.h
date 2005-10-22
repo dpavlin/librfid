09a3dd030ddac3875b865be72e91d8326367dd4c
laforge <laforge@e0336214-984f-0b4b-a45f-81c69e1f0ede>
2005-10-22 17:55:57 +0000

    mifare ultraligh have pages 0-15, not only 0-7
    git-svn-id: https://svn.gnumonks.org/trunk/librfid@1552 e0336214-984f-0b4b-a45f-81c69e1f0ede

--- include/rfid/rfid_protocol_mifare_ul.h
+++ include/rfid/rfid_protocol_mifare_ul.h
@@ -1,6 +1,15 @@
 #ifndef _RFID_PROTOCOL_MFUL_H
 #define _RFID_PROTOCOL_MFUL_H
 
+
+#define MIFARE_UL_CMD_WRITE	0xA2
+#define MIFARE_UL_CMD_READ	0x30
+
+#define MIFARE_UL_RESP_ACK	0x0a
+#define MIFARE_UL_RESP_NAK	0x00
+
+#define MIFARE_UL_PAGE_MAX	15
+
 struct rfid_protocol rfid_protocol_mful;
 
 #endif
