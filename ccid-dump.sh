587eec933bcbfd3a4909311777aa46dbef351dd7
Dobrica Pavlinusic <dpavlin@rot13.org>
2014-01-19 09:12:42 -0500

    run dump as root to side-step permission problems

--- ccid-dump.sh
+++ ccid-dump.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -xe
 test -d dump || mkdir dump
 test -f dump/ccid || gcc -DTEST -Wall -Iinclude -lusb -g src/ccid/ccid-driver.c -o dump/ccid
-./dump/ccid 2>&1 | tee dump/`hostname`.ccid
+sudo ./dump/ccid 2>&1 | tee dump/`hostname`.ccid
