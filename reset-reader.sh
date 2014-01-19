a50f7e4b3559b650f7c95f6320a86263b08feb53
Dobrica Pavlinusic <dpavlin@rot13.org>
2014-01-19 09:36:03 -0500

    remove timestamp and then parse dmesg

--- reset-reader.sh
+++ reset-reader.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -xe
 
 #dev=`lsusb | grep 076b:5.21 | awk '{ print $2 "-" $4 }' | sed -e 's/0//g' -e 's/://'`
-dev=`dmesg | grep OMNIKEY | cut -d" " -f3 | cut -d: -f1 | tail -1`
+dev=`dmesg | grep OMNIKEY | sed 's/\[.*\] //'  | cut -d" " -f2 | cut -d: -f1 | tail -1`
 authorized=/sys/bus/usb/devices/$dev/authorized
 test -w $authorized || exit 1
 echo 0 > $authorized
