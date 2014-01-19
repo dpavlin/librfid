7bc585e169feebfb94f39634245f557f0219e5b2
Dobrica Pavlinusic <dpavlin@rot13.org>
2014-01-19 15:20:08 +0100

    extract device from dmesg

--- reset-reader.sh
+++ reset-reader.sh
@@ -1,8 +1,11 @@
 #!/bin/sh -xe
 
-dev=`lsusb | grep 076b:5.21 | awk '{ print $2 "-" $4 }' | sed -e 's/0//g' -e 's/://'`
+#dev=`lsusb | grep 076b:5.21 | awk '{ print $2 "-" $4 }' | sed -e 's/0//g' -e 's/://'`
+dev=`dmesg | grep OMNIKEY | cut -d" " -f3 | cut -d: -f1 | tail -1`
 authorized=/sys/bus/usb/devices/$dev/authorized
+test -w $authorized || exit 1
 echo 0 > $authorized
-dmesg
+dmesg | tail -2
+sleep 1
 echo 1 > $authorized
-dmesg
+dmesg | tail -2
