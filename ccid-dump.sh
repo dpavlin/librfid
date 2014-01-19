25edebd1af6fd0dd91b870ccc57002a9f477c263
Dobrica Pavlinusic <dpavlin@rot13.org>
2014-01-19 15:50:33 +0100

    enable debug output and disable poll for smartcard

--- ccid-dump.sh
+++ ccid-dump.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -xe
 test -d dump || mkdir dump
-test -f dump/ccid || gcc -DTEST -Wall -Iinclude -lusb -g src/ccid/ccid-driver.c -o dump/ccid
-sudo ./dump/ccid 2>&1 | tee dump/`hostname`.ccid
+#test -f dump/ccid || 
+gcc -DTEST -Wall -Iinclude -lusb -g src/ccid/ccid-driver.c -o utils/ccid
+sudo ./utils/ccid --debug --no-poll 2>&1 | tee dump/`hostname`.ccid
