361596ac0717ca9e20a026de456b5728f7b9ac9e
Dobrica Pavlinusic <dpavlin@rot13.org>
2014-01-19 10:00:19 -0500

    generate unique reader.host.ccid files

--- ccid-dump.sh
+++ ccid-dump.sh
@@ -2,4 +2,6 @@
 test -d dump || mkdir dump
 #test -f dump/ccid || 
 gcc -DTEST -Wall -Iinclude -lusb -g src/ccid/ccid-driver.c -o utils/ccid
-sudo ./utils/ccid --debug --no-poll 2>&1 | tee dump/`hostname`.ccid
+./utils/ccid --debug --no-poll 2>&1 | tee dump/ccid
+id=`head dump/debian7.ccid | cut -d: -f4 | head -1`
+mv dump/ccid dump/$id.`hostname`.ccid
