ac14895e9f07e619f01ed0098e88663ff83555d5
Dobrica Pavlinusic <dpavlin@rot13.org>
2014-01-19 09:02:15 -0500

    install udev

--- debian-install.sh
+++ debian-install.sh
@@ -4,3 +4,6 @@ sudo apt-get -y install autoconf automake libtool gcc make libusb-dev
 ./autogen.sh
 ./configure --enable-ccid
 make
+sudo make install
+sudo cp etc/udev/librfid.rules /etc/udev/rules.d/
+sudo /etc/init.d/udev restart
